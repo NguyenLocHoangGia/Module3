d1241ed1cce54b814d5c48b27a837fdb827f7602
NguyenLocHoangGia <hoanggia09052004@gmail.com>
2023-08-22 17:59:23 +0700

    Baitap

---
+++ Bai1/bai1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `student-management`.`student` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+SELECT * FROM `student-management`.student;
---
+++ Bai1/teacher.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `student-management`.`teacher` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `age` INT NULL,
+  `country` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+SELECT * FROM `student-management`.teacher;
