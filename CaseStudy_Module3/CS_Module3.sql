debce2f66b01cfb47293148a1d2b3cf3a62b94b1
NguyenLocHoangGia <hoanggia09052004@gmail.com>
2023-08-30 21:26:05 +0700

    baitap

--- CaseStudy_Module3/CS_Module3.sql
+++ CaseStudy_Module3/CS_Module3.sql
@@ -171,13 +171,13 @@ values ('Đánh goft',5000,'giờ','tốt'),
 ('Ăn uống',1000, 'suất', 'tốt');
 
 Insert into hop_dong(ngay_lam_hop_dong,ngay_ket_thuc,tien_dat_coc,ma_nhan_vien,ma_khach_hang,ma_dich_vu) 
-values('2020-07-14','2020-07-21',200000,7,3,1),
-('2021-06-01','2021-06-03',0,7,7,6),
-('2021-06-17','2021-06-18',150000,3,4,1),
-('2021-05-25','2021-05-27',0,7,10,1);
+values('2020-07-14','2020-07-21',200000,1,1,1),
+('2021-06-01','2021-06-03',0,2,2,2),
+('2021-06-17','2021-06-18',150000,3,3,3),
+('2021-05-25','2021-05-27',0,4,4,4);
 
 Insert into hop_dong_chi_tiet(so_luong,ma_hop_dong,ma_dich_vu_di_kem) 
- values(5,2,4),(8,2,5),(15,2,6),(1,3,1),(11,3,1),(1,1,3),(2,1,2),(2,12,2);
+ values(5,2,4),(8,2,3),(15,2,2),(1,3,1),(11,3,1),(1,1,3),(2,1,2),(2,12,2);
 
 --  2.Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
  select * from nhan_vien where (ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%') and length(ho_ten) <= 15;
@@ -200,3 +200,76 @@ select K.ma_khach_hang, K.ho_ten, Lk.ten_loai_khach,count(1) as so_lan_dat from 
  left join dich_vu DV on DV.ma_dich_vu = H.ma_dich_vu
  left join hop_dong_chi_tiet HDCT on HDCT.ma_hop_dong = H.ma_hop_dong
  left join dich_vu_di_kem DVDK on DVDK.ma_dich_vu_di_kem = HDCT.ma_dich_vu_di_kem;
+ 
+ -- 6.Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được 
+ -- khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+ 
+ SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+FROM dich_vu dv
+INNER JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+WHERE dv.ma_dich_vu NOT IN (
+    SELECT DISTINCT hd.ma_dich_vu
+    FROM hop_dong hd
+    WHERE hd.ngay_lam_hop_dong >= '2021-01-01' AND hd.ngay_lam_hop_dong <= '2021-03-31'
+);
+
+-- 7.Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da,
+-- chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.so_nguoi_toi_da, dv.chi_phi_thue, ldv.ten_loai_dich_vu
+FROM dich_vu dv
+INNER JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+WHERE dv.ma_dich_vu IN (
+    SELECT DISTINCT hd.ma_dich_vu
+    FROM hop_dong hd
+    WHERE YEAR(hd.ngay_lam_hop_dong) = 2020
+)
+AND dv.ma_dich_vu NOT IN (
+    SELECT DISTINCT hd.ma_dich_vu
+    FROM hop_dong hd
+    WHERE YEAR(hd.ngay_lam_hop_dong) = 2021
+);
+
+-- 8.Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+
+-- cách 1
+SELECT DISTINCT ho_ten
+FROM khach_hang;
+-- cách 2
+SELECT ho_ten
+FROM khach_hang
+GROUP BY ho_ten;
+-- cách 3
+SELECT ho_ten
+FROM (
+    SELECT ho_ten, ROW_NUMBER() OVER (PARTITION BY ho_ten ORDER BY ma_khach_hang) AS row_num
+    FROM khach_hang
+) ranked
+WHERE row_num = 1;
+
+-- 9.Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+SELECT MONTH(ngay_lam_hop_dong) AS thang, COUNT(DISTINCT ma_khach_hang) AS so_luong_khach_hang
+FROM hop_dong
+WHERE YEAR(ngay_lam_hop_dong) = 2021
+GROUP BY thang
+ORDER BY thang;
+
+-- 10.Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc,
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+SELECT
+    hd.ma_hop_dong,
+    hd.ngay_lam_hop_dong,
+    hd.ngay_ket_thuc,
+    hd.tien_dat_coc,
+    SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem
+FROM
+    hop_dong hd
+JOIN
+    hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+GROUP BY
+    hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc
+ORDER BY
+    hd.ma_hop_dong;
